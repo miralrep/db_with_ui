228c2f43fc12fc600a3810b6e36a56dcc627d4d4
Marach <Marach@lanit.ru>
2021-05-19 15:31:29 +0300

    Создал бд

---
+++ src/main/resources/database.sql
@@ -0,0 +1,75 @@
+--subscriber
+--id|surname|name|patronymic|address|privileges
+CREATE TABLE IF NOT EXISTS subscriber
+(
+    id              SERIAL PRIMARY KEY NOT NULL,
+    surname         VARCHAR(100) NOT NULL,
+    name            VARCHAR(100) NOT NULL,
+    patronymic      VARCHAR(100) NOT NULL,
+    address         VARCHAR(200) NOT NULL
+);
+
+--subscriber
+--id|name|discount
+CREATE TABLE IF NOT EXISTS privilege
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    discount DECIMAL(5,2)
+);
+
+CREATE TABLE subscriber_privilege
+(
+    subscriber_id   INT NOT NULL,
+    privilege_id      INT NOT NULL,
+
+    FOREIGN         KEY(subscriber_id) REFERENCES subscriber(id)
+
+);
+-------------------------------------------------------------
+--category
+--id|name|subscription_fee
+CREATE TABLE IF NOT EXISTS category
+(
+    id              SERIAL PRIMARY KEY NOT NULL,
+    name            VARCHAR(100) NOT NULL,
+    subscription_fee DECIMAL(7,2) NOT NULL
+);
+-------------------------------------------------------------
+--phone_number
+--number|subscriber_id|category_id
+CREATE TABLE IF NOT EXISTS phone_number
+(
+    number          VARCHAR(50) NOT NULL PRIMARY KEY ,
+    subscriber_id   INT NOT NULL,
+    category_id     INT NOT NULL,
+
+    FOREIGN KEY(subscriber_id) REFERENCES subscriber(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+-------------------------------------------------------------
+
+
+CREATE TABLE IF NOT EXISTS call_type
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    category_id INT NOT NULL,
+    fee DECIMAL(5,2),
+
+    FOREIGN KEY(category_id) REFERENCES category(id)
+);
+--conversation
+--calling_phone|taking_phone|type|duration|date
+CREATE TABLE IF NOT EXISTS conversation
+(
+    id              SERIAL PRIMARY KEY NOT NULL,
+    calling_phone   VARCHAR(50) NOT NULL,
+    taking_phone    VARCHAR(50) NOT NULL,
+    call_type_id            INT NOT NULL,
+    duration        TIMESTAMP NOT NULL,
+    date            INTERVAL NOT NULL,
+
+    FOREIGN KEY(call_type_id) references call_type(id)
+);
+-------------------------------------------------------------
---
+++ src/main/resources/databaseDropTables.sql
@@ -0,0 +1,7 @@
+DROP TABLE subscriber CASCADE ;
+DROP TABLE subscriber_privilege CASCADE;
+DROP TABLE privilege CASCADE;
+DROP TABLE category CASCADE;
+DROP TABLE conversation CASCADE;
+DROP TABLE phone_number CASCADE;
+DROP TABLE call_type CASCADE;
